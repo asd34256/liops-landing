ea161c07be9128bb44c614258c6b8e18dedb13f8
asd34256 <ara34256@snu.ac.kr>
2023-12-21 17:26:46 +0900

    fix: modify Link to PageLink

--- site.config.ts
+++ site.config.ts
@@ -40,11 +40,11 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/about': 'b0fbc4aad79148d0848485e4beb6520d',
+    '/contact': 'b0fbc4aad79148d0848485e4beb6520d'
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
@@ -53,11 +53,11 @@ export default siteConfig({
   navigationLinks: [
     {
       title: 'About',
-      pageId: '32bc02d0f32b4a37aabdbac0917b1533'
+      url: '/about'
     },
     {
       title: 'Contact',
-      pageId: '32bc02d0f32b4a37aabdbac0917b1533'
+      url: '/contact'
     }
   ]
 })
